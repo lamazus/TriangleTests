e31767c0e5529b4c8b197f0fe1aea5d0bf4b83b0
lamazus <flooop08@gmail.com>
2023-09-11 20:03:07 +0300

    removed redundant test

--- tests/test_triangle_page.py
+++ tests/test_triangle_page.py
@@ -24,7 +24,7 @@ class TestTianglePage():
     @pytest.mark.parametrize("a, b, c", 
                              [
                                  (0,0,0), ("a", 5, 6), (2, "b", 4), 
-                                 (4, 4, "c"), ("a", "b", "c"), (3.4, 4.1, 4.9),
+                                 (4, 4, "c"), ("a", "b", "c"),
                                  (3000000000, 3000000000, 3000000000), ('','',''),
                                  ("SELECT * FROM users", 1, 2),
                                  ("<Script>alert('Hacked?')</Script>", 3, 4)])
